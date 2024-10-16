037867d1e517c058bc191ef00a94fa39c5326b01
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-10-17 00:09:39 +0700

    Menambahkan komponen Teks dengan isi "Login" serta font size berukuran 50 sp dan font tebal

--- app/src/main/java/com/example/questbasiccomposable_102/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_102/MainActivity.kt
@@ -15,7 +15,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_102.ui.theme.QuestBasicComposable_102Theme
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +45,7 @@ fun BasicLayout(modifier: Modifier = Modifier) { //Class Basic Layout untuk memb
         verticalArrangement = Arrangement.Center, // Semua elemen dalam kolom diatur center atau di tengah secara vertikal
         modifier = Modifier.fillMaxSize() // Untuk Memenuhi seluruh layout
     ) {
-
+        Text(text = "Login", style = TextStyle(fontSize = 50.sp, fontWeight = FontWeight.Bold)) // Menampilkan Teks
     }
 }
 
