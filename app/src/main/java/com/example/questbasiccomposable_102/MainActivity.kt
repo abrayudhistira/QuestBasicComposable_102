28675436192374a6f4d4296f34d1ce6d4c8e73e9
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-10-16 23:45:07 +0700

    Membuat Widget Surface

--- app/src/main/java/com/example/questbasiccomposable_102/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_102/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,11 +21,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuestBasicComposable_102Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(        //Membuat Widget Surface
+
+                ) {
+
                 }
             }
         }
@@ -31,17 +32,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BasicLayout(modifier: Modifier = Modifier) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     QuestBasicComposable_102Theme {
-        Greeting("Android")
+        BasicLayout()
     }
 }
